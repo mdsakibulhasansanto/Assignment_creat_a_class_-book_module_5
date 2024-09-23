3aad04293630aedf2d969cc0c944b624212dd66c
md_santo <mdsakibulhasansanto7@gmail.com>
2024-09-23 20:14:25 +0600

    update

---
+++ bin/creat_a_class__book_module_5.dart
@@ -0,0 +1,5 @@
+import 'package:creat_a_class__book_module_5/creat_a_class__book_module_5.dart' as creat_a_class__book_module_5;
+
+void main(List<String> arguments) {
+  print('Hello world: ${creat_a_class__book_module_5.calculate()}!');
+}
---
+++ test/creat_a_class__book_module_5_test.dart
@@ -0,0 +1,8 @@
+import 'package:creat_a_class__book_module_5/creat_a_class__book_module_5.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
